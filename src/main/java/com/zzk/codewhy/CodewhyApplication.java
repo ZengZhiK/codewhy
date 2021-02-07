788a810dd11ac0727de4e2cf003b12a1b0b54771
ZengZhiK <BUCTZengZK@163.com>
2021-02-07 16:28:33 +0800

    First Commit

---
+++ src/main/java/com/zzk/codewhy/CodewhyApplication.java
@@ -0,0 +1,13 @@
+package com.zzk.codewhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewhyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodewhyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzk/codewhy/CodewhyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzk.codewhy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewhyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
